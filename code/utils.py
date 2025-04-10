006f37e3b160ad080387465e0ddbef09b987f9ab
sgoutteb <93519747+sgoutteb@users.noreply.github.com>
2025-04-10 08:51:27 +0200

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -6,7 +6,7 @@
 #
 # functions for segmata
 #
-# version 04.04.2025
+# version 07.04.2025
 # S.Gouttebroze
 #----------------------------------------------
 
@@ -18,6 +18,7 @@ import subprocess
 import shutil
 import matplotlib.pyplot as plt
 from datetime import datetime
+import re
 
 def calc_diff(image1_path, image2_path, display=False):
     image1 = cv2.imread(image1_path, cv2.IMREAD_GRAYSCALE)
@@ -43,7 +44,7 @@ def calc_diff(image1_path, image2_path, display=False):
     return np.mean(image_diff)
 
 def load_obj(obj_file_path, obj_filename):
-    vertices, normals, faces, others = [], [], [], []
+    vertices, normals, faces, comments, others = [], [], [], [], []
     with open(os.path.join(obj_file_path, obj_filename), "r") as f:
         for line in f:
             parts = line.strip().split()
@@ -55,12 +56,16 @@ def load_obj(obj_file_path, obj_filename):
                 normals.append([float(x) for x in parts[1:4]])
             elif parts[0] == "f":
                 faces.append(line.strip())
+            elif parts[0] == "#":
+                comments.append(line.strip())
             else:
                 others.append(line.strip())
-    return vertices, normals, faces, others
+    return vertices, normals, faces, comments, others
 
-def save_obj(vertices, normals, faces, others, path):
+def save_obj(vertices, normals, faces, comments, others, path):
     with open(path, "w") as f:
+        for line in comments:
+            f.write(line + "\n")
         for v, n in zip(vertices, normals):
             f.write(f"v {v[0]} {v[1]} {v[2]}\n")
             f.write(f"vn {n[0]} {n[1]} {n[2]}\n")
@@ -98,13 +103,34 @@ def display_result(output_image,image_reference):
     axes[1].axis('off')
     plt.tight_layout()
     plt.show()
+    
+    
+def display_result_vertex(vertex_number,vertex_disp):
+    plt.figure(figsize=(10, 5))
+
+    for i in range(len(vertex_number)):
+        plt.plot(vertex_number[i], vertex_disp[i], marker='o', linestyle='-', label=f'Pass {i+1}')
+
+    plt.xlabel("Vertex Number")
+    plt.ylabel("Displacement")
+    plt.title("Displacement vs Vertex Number")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+    
             
 def segmata(log_path,obj_file_path, obj_filename, render_exe, render_args, output_image, image_reference, distance,pass_number):
-    vertices, normals, faces, others = load_obj(obj_file_path, obj_filename)
+    vertex_number=[]
+    vertex_disp=[]
+    vertices, normals, faces, comments, others = load_obj(obj_file_path, obj_filename)
     base_path = os.path.join(obj_file_path, obj_filename)
     shutil.copy(base_path, base_path + ".bak")
+    
+    # test
+    #extract_faces_with_adjacency(vertices,normals, faces, comments, others, 10, obj_file_path+"extract_x.obj")
 
-    save_obj(vertices, normals, faces, others, os.path.join(obj_file_path, "temp_" + obj_filename))
+    save_obj(vertices, normals, faces, comments, others, os.path.join(obj_file_path, "temp_" + obj_filename))
     subprocess.run([render_exe] + render_args, stdout=sys.stdout, stderr=sys.stderr)
     shutil.copy(output_image, output_image + "_ref.jpg")
     if pass_number==0:
@@ -118,7 +144,7 @@ def segmata(log_path,obj_file_path, obj_filename, render_exe, render_args, outpu
             moved_vertices[i] = move_vertex(vertices, normals, i, direction * distance)
 
             temp_obj_path = os.path.join(obj_file_path, f"temp_{i}_{'p' if direction > 0 else 'm'}.obj")
-            save_obj(moved_vertices, normals, faces, others, temp_obj_path)
+            save_obj(moved_vertices, normals, faces, comments, others, temp_obj_path)
 
             render_args_mod = render_args.copy()
             render_args_mod[render_args_mod.index("--obj") + 1] = temp_obj_path
@@ -131,18 +157,27 @@ def segmata(log_path,obj_file_path, obj_filename, render_exe, render_args, outpu
             if diff > 0:
                 #shutil.copy(temp_obj_path, base_path + f"_{i}_{'p' if direction > 0 else 'm'}.obj")
                 #shutil.copy(output_image, output_image + f"_ref_{i}_{'p' if direction > 0 else 'm'}.jpg")
-                save_obj(moved_vertices, normals, faces, others, base_path)
+                save_obj(moved_vertices, normals, faces, comments, others, base_path)
                 shutil.copy(output_image, output_image + "_ref.jpg")
                 vertices = moved_vertices
                 log_print(log_path,f"Vertex {i+1}/{len(vertices)} moved by {'+' if direction > 0 else '-'}{distance} pixels)")
                 cpt += 1
+                vertex_number.append(i)
+                vertex_disp.append(direction * distance)
+                
                 if os.path.exists(temp_obj_path): os.remove(temp_obj_path)  # Suppress file
                 break
             if os.path.exists(temp_obj_path): os.remove(temp_obj_path)  # Suppress file
+            
+    shutil.copy(output_image, output_image + f"_opt_pass{pass_number+1}.jpg")
 
     log_print(log_path,"****************************")
-    log_print(log_path,"  OPTIMIZATION DONE.")
+    log_print(log_path,f"  OPTIMIZATION PASS {pass_number+1} DONE.")
     log_print(log_path,f"Vertex moved:")
     log_print(log_path,f"{cpt} of {len(vertices)} vertex")
     log_print(log_path,f"{round(100*cpt/len(vertices),0)} % of total")
     log_print(log_path,"****************************")
+    
+    return [vertex_number,vertex_disp]
+
+
